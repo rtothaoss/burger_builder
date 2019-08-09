b188df97d92778f8ea104ab3e4075c8b348e416b
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-08-09 13:58:24 -0500

    fixed backdrop showing on first load of page

--- client/src/components/Burger/OrderSummary/OrderSummary.js
+++ client/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,14 +1,17 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import Button from '../../UI/Button/Button'
 
 
-const OrderSummary = (props) => {
+class OrderSummary extends Component {
+    //this could be a functional component instead of class
 
-    const ingredientSummary = Object.keys(props.ingredients)//transforms object into keys
+    render() {
+        
+    const ingredientSummary = Object.keys(this.props.ingredients)//transforms object into keys
         .map(igKey => {
             return (
             <li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}:</span>{props.ingredients[igKey]}
+            <span style={{textTransform: 'capitalize'}}>{igKey}:</span>{this.props.ingredients[igKey]}
             </li> )
         } )
     return (
@@ -18,12 +21,13 @@ const OrderSummary = (props) => {
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType={'Danger'} clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType={'Success'} clicked={props.purchaseContinue}>CONTINUE</Button>
+            <Button btnType={'Danger'} clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType={'Success'} clicked={this.props.purchaseContinue}>CONTINUE</Button>
         </Fragment>
     )
+}
 }
 
 export default OrderSummary
--- client/src/components/UI/Modal/Modal.js
+++ client/src/components/UI/Modal/Modal.js
@@ -1,22 +1,36 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import Backdrop from '../Backdrop/Backdrop'
 import classes from './Modal.module.css'
 
 
-const Modal = (props) => (
-    <Fragment>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-        >
-            {props.children}
-        </div>
-    </Fragment>
-)
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+    }
+
+    componentDidMount () {
+        console.log('[Modal] Will Update')
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Fragment>
+        )
+    }
+
+}
 
 
 
