98cab90df74395fc8f20ef7eb1f3314a176f82c5
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-08-09 12:03:37 -0500

    added mobile responsiveness and burger menu icon

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,15 +1,35 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import classes from './Layout.module.css'
 
-const Layout = (props) => {
-    return (
-        <Fragment>
-            <div>Toolbar, SideDrawer, Backdrop</div>
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Fragment>
-    )
+class Layout extends Component {
+    
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        )
+    }
 }
 
 export default Layout
--- client/src/components/Navigation/Toolbar/Toolbar.js
+++ client/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import classes from './Toolbar.module.css'
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 
 const Toolbar = (props) => (
-    <header>
-        <div>MENU</div>
+    <header className={classes.Toolbar}>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+        </nav>
     </header>
 )
 
