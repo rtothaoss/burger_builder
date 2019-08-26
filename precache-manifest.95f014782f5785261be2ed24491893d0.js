68a96e311f3a127956b3892059e2e0452640a413
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-08-26 14:41:56 -0500

    Updates

---
+++ precache-manifest.95f014782f5785261be2ed24491893d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33eff215822f5d07350d7ecc84ca8edb",
+    "url": "/burger_builder/index.html"
+  },
+  {
+    "revision": "0c8ef4e685ccb7f5fe79",
+    "url": "/burger_builder/static/css/main.a8354088.chunk.css"
+  },
+  {
+    "revision": "3cdfb65f7b3a305e4bda",
+    "url": "/burger_builder/static/js/2.db98fae4.chunk.js"
+  },
+  {
+    "revision": "0c8ef4e685ccb7f5fe79",
+    "url": "/burger_builder/static/js/main.22c77d74.chunk.js"
+  },
+  {
+    "revision": "fd3cde94dfbe7ffa1088",
+    "url": "/burger_builder/static/js/runtime~main.bef5c481.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
