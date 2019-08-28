ae6f358e0d074112a9a5a63024fa695d675b2525
Ross carmack <rosscarmack@Rosss-MBP.fios-router.home>
2019-08-28 11:53:51 -0500

    updating project before error handling jest problem

--- client/src/components/UI/Modal/Modal.js
+++ client/src/components/UI/Modal/Modal.js
@@ -8,11 +8,7 @@ class Modal extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         return nextProps.show !== this.props.show || nextProps.children !== this.props.children
     }
-
-    componentDidMount () {
-        console.log('[Modal] Will Update')
-    }
-
+    
     render() {
         return (
             <Fragment>
